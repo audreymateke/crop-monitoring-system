9899f2544f79f6e73302c6575d87648e97b41b63
audrey <audreymateke1@gmail.com>
2024-09-30 13:37:53 +0200

    first commit

---
+++ crop-segmentation/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Creating a system that can seperate corn crops from aerial images and corn counts and health informations',
+    author='agxone-projects',
+    license='MIT',
+)
